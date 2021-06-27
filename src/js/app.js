6d1efe73ab405f99ce5408959f0ce9d3669a6577
Nadezhda Zizevskaya <7_nadezhda@mail.ru>
2021-06-27 20:32:19 +0400

    second commit

--- src/js/app.js
+++ src/js/app.js
@@ -1,15 +1,15 @@
 export default function orderByProps(objectSort, arrSort = []) {
   let arrAllKey = [];
-    for (const key in objectSort) {
-      if (arrSort.indexOf(key) === -1) {
-        arrAllKey.push(key);
-      }
+  for (const key in objectSort) {
+    if (arrSort.indexOf(key) === -1) {
+      arrAllKey.push(key);
     }
-    arrAllKey = [...arrSort, ...arrAllKey.sort()];
-  
+  }
+  arrAllKey = [...arrSort, ...arrAllKey.sort()];
   const result = [];
-    for (const key of arrAllKey) {
-      result.push({ key, value: objectSort[key] });
-    }
-    return result;
+  for (const key of arrAllKey) {
+    result.push({ key, value: objectSort[key] });
+  }
+  return result;
 }
+
